944de3e3db86e37955eb5a983423f63971cd696b
Adam Mausenbaum <adam@admm.io>
2017-03-02 15:01:42 +0200

    add example of state expect

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow} from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('should have some initial state', () => {
+  const wrapper = shallow(<App/>);
+  expect(wrapper.state().selectedProducts.length).toEqual(0);
 });
